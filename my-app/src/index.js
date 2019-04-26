974faef4fc30f4c2f8b383754b8e494b6077f877
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-04-26 10:32:30 +0200

    Firebase add to React

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import App from './components/App';
+
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 ReactDOM.render(
-		<App/>,
-	document.getElementById("root")
+	<FirebaseContext.Provider value={new Firebase()}>
+		<App />
+	</FirebaseContext.Provider>,
+	document.getElementById('root'),
 );
-serviceWorker.register();
+
+serviceWorker.unregister();
