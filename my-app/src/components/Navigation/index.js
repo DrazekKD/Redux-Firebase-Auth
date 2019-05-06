b7a7cdab5929b985c4106711f5b86ff4952fed72
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-05-06 08:35:12 +0200

    password change password reset

--- my-app/src/components/Navigation/index.js
+++ my-app/src/components/Navigation/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import {Link} from 'react-router-dom';
+import {AuthUserContext} from '../Session';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = ({ authUser }) => (
-	<div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+const Navigation = () => (
+	<div>
+		<AuthUserContext.Consumer>
+			{authUser =>
+				authUser ? <NavigationAuth/> : <NavigationNonAuth/>
+			}
+		</AuthUserContext.Consumer>
+	</div>
 );
 
 const NavigationAuth = () => (
@@ -20,7 +26,7 @@ const NavigationAuth = () => (
 			<Link to={ROUTES.ACCOUNT}>Account</Link>
 		</li>
 		<li>
-			<SignOutButton />
+			<SignOutButton/>
 		</li>
 	</ul>
 );
