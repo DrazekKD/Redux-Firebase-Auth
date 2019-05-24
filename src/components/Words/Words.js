ba70897367d8cbd8699e010ade94cc4a057ac511
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-05-24 10:01:01 +0200

    translate Google Rest API

--- src/components/Words/Words.js
+++ src/components/Words/Words.js
@@ -1,68 +1,71 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { compose } from 'recompose';
 
 import {AuthUserContext, withAuthorization, withEmailVerification} from '../Session';
 import { withFirebase } from '../Firebase';
-import WordsTranslation from './WordsTranslation'
-import {googleTranslate } from '../../operations/operations'
 class Words extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			text: '',
-			translateText:''
+			translateText:'',
+			sourceLanguage:'pl',
+			targetLanguage:'en'
 		};
 	}
-	componentDidMount() {
-		this.onListenForMessages();
-	}
-
-	componentDidUpdate(props) {
-		if (props.limit !== this.props.limit) {
-			this.onListenForMessages();
-		}
-	}
-
-	componentWillUnmount() {
-		this.props.firebase.userWords().off();
-	}
-	onListenForMessages = () => {
-		this.props.firebase
-			.userWords(this.props.authUser.uid)
-			.orderByChild('createdAt')
-			.on('value', snapshot => {
-				this.props.onSetWords(snapshot.val());
-			});
-	};
+	// componentDidMount() {
+	// 	this.onListenForMessages();
+	// }
+	//
+	// componentDidUpdate(props) {
+	// 	if (props.limit !== this.props.limit) {
+	// 		this.onListenForMessages();
+	// 	}
+	// }
+	//
+	// componentWillUnmount() {
+	// 	this.props.firebase.userWords().off();
+	// }
+	// onListenForMessages = () => {
+	// 	this.props.firebase
+	// 		.userWords(this.props.authUser.uid)
+	// 		.orderByChild('createdAt')
+	// 		.on('value', snapshot => {
+	// 			this.props.onSetWords(snapshot.val());
+	// 		});
+	// };
 	onCreateMessage = (event) => {
 		this.props.firebase.userWords(this.props.authUser.uid).push({
 			text: this.state.text,
 			createdAt: this.props.firebase.serverValue.TIMESTAMP,
-			translation: '',
+			translation: this.state.translateText,
 			language: ''
 		});
-		this.setState({ text: '' });
+		this.setState({ text: '',translateText:'' });
 		event.preventDefault();
 	};
 	onChangeText = event => {
 		this.setState({ text: event.target.value });
 	};
-
-	onTranslate = () =>{
-		console.log(this.state.text);
-		googleTranslate.translate( this.state.text, 'pl', function(err, translation) {
-
-			console.log(translation)
-		});
+	onChangeTranslateText = event => {
+		this.setState({ translateText: event.target.value });
+	};
+	onTranslate = (text,sourceLanguage,targetLanguage) =>{
+		fetch(`https://translation.googleapis.com/language/translate/v2
+					?key=${process.env.REACT_APP_API_KEY_GOOGLE_TRANSLATION}
+					&q=${text}
+					&source=${sourceLanguage}
+					&target=${targetLanguage}`)
+			.then(res => res.json())
+			.then(json =>this.setState(
+				{translateText: json.data.translations[0].translatedText}));
 	};
 
 	render() {
-		const { text } = this.state;
+		const { text, translateText, targetLanguage ,sourceLanguage} = this.state;
 		return (
 			<div>
-				{/*<WordsTranslation/>*/}
 				<form
 					onSubmit={event =>
 						this.onCreateMessage(event)
@@ -71,31 +74,22 @@ class Words extends Component {
 						type="text"
 						value={text}
 						onChange={this.onChangeText}/>
-					<button type="button" onClick={this.onTranslate}>Translate</button>
-					<button type="submit">Send</button>
+					<input
+						type="text"
+						value={translateText}
+						onChange={this.onChangeTranslateText}/>
+					<button type="button" onClick={()=>this.onTranslate(text,sourceLanguage,targetLanguage)}>Translate</button>
+					<button type="submit" disabled={!(!!translateText && !!text)}>Send</button>
 				</form>
 			</div>
 		)
 	}
 }
 
-const mapStateToProps = state => ({
-	words: state.wordsState.words,
-});
-
-const mapDispatchToProps = dispatch => ({
-	onSetWords: words =>
-		dispatch({ type: 'WORDS_SET', words }),
-});
-
 const condition = authUser => !!authUser;
 
 export default compose(
 	withFirebase,
-	connect(
-		mapStateToProps,
-		mapDispatchToProps,
-	),
 	withEmailVerification,
 	withAuthorization(condition),
 )(Words);
